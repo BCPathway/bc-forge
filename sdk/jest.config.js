5d4eed5df675c2a723f0ac132e0b1f612d4b4514
Promise Raji <isaacsamson88@gmail.com>
2026-07-24 08:52:50 +0100

    chore: fixing CI

--- sdk/jest.config.js
+++ sdk/jest.config.js
@@ -2,4 +2,11 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
+  transformIgnorePatterns: [],
+  transform: {
+    '^.+\\.[jt]sx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 };
